9498bdd3d56c86da6079f9eff8f474148010f2e8
Allegra Saggese <111875712+allegra-saggese-econ@users.noreply.github.com>
2025-08-11 15:27:32 +0100

    create script for import ag data

---
+++ script0b-ag-raw.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 11 15:19:00 2025
+
+@author: allegrasaggese
+"""
+
+# load packages and workspaces
+import sys, importlib.util
+from collections import Counter
+import re
+
+# make sure repo root is on sys.path (parent of functions.py / packages/)
+repo = "/Users/allegrasaggese/Documents/GitHub/mentalhealth"
+if repo not in sys.path:
+    sys.path.append(repo)
+
+import functions       
+import packages         
+
+print("functions file:", functions.__file__)
+print("packages file:",  packages.__file__) 
+
+from functions import *     
+from packages import *
+
+
+# other folders
+inf = os.path.join(db_data, "raw") # input 
+outf = os.path.join(db_data, "clean") #outpit
