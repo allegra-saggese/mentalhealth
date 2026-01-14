de75d7602404fa2514719c9b168cd025b8204029
Allegra Saggese <allegrasaggese@hotmail.com>
2026-01-14 11:58:04 -0800

    add new data in
    - add in slaughterhouse data + script
    - add in rural/urban classification + script
    - rename seq (1) scripts --- for QA, merging, and aggregation of final panel
    - rename seq(2) for visualization and data exploration pre-analysis

---
+++ script0e-fsis-slaughterhouses.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 14 11:54:38 2026
+
+@author: allegrasaggese
+"""
+
+# read in new augmentations to the data - slaughterhouses 
---
+++ script0f-nchs-urban.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 14 11:55:22 2026
+
+@author: allegrasaggese
+"""
+
