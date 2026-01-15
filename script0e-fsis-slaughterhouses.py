c8d04ed892010e2a8719f62166a175cb4b1c5747
Allegra Saggese <allegrasaggese@hotmail.com>
2026-01-14 16:52:40 -0800

    add in slaughterhouse data
    -note we need to investigate the time on the slaughterhouse data, we only have 2025 inspection data

--- script0e-fsis-slaughterhouses.py
+++ script0e-fsis-slaughterhouses.py
@@ -7,3 +7,11 @@ Created on Wed Jan 14 11:54:38 2026
 """
 
 # read in new augmentations to the data - slaughterhouses 
+inf = os.path.join(db_data, "raw") # input 
+outf = os.path.join(db_data, "clean") #output
+
+
+# FUTURE CHANGE: JAN 2026 - should change to use API to download relevant data only 
+# import ag data 
+fsis_data = os.path.join(inf, "fsis")
+fsis_df = pd.read_csv(os.path.join(fsis_data, "2026-updated-Dataset_Establishment_Demographic_Data.csv"))
